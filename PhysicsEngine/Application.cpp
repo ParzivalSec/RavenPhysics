b1c960152731a635cb730c707ecd85705256c718
Lukas Vogl <lukas_vienna@hotmail.com>
2017-06-20 19:04:15 +0200

    Removed Millington Architecture based source

--- PhysicsEngine/Application.cpp
+++ PhysicsEngine/Application.cpp
@@ -1,56 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include "Clock.h"
-#include "Particle.h"
-#include "ParticleForceRegistry.h"
-#include "FanForceGenerator.h"
-#include "ParticleSpring.h"
-#include "GravitationForceGenerator.h"
 
 int main()
 {
 	sf::VideoMode mode(1028, 720);
 	sf::RenderWindow window(mode, "Physics Engine @ Lukas Vogl");
 
-	raven::ParticleForceRegistry registry;
-	std::vector<raven::Particle*> particles;
-
-	raven::GravitationForceGenerator gravitation(glm::vec2(0.0f, 10.0f));
-	raven::FanForceGenerator fan(glm::vec2(1.0f, 0.0f), 0.0f);
-
-	// Create first particle
-	raven::Particle* part1 = new raven::Particle(glm::vec2(10.0f, 10.0f), glm::vec2(0.0f, 0.0f));
-	part1->SetMass(1.0f);
-	particles.push_back(part1);
-
-	raven::Particle* part2 = new raven::Particle(glm::vec2(30.0f, 11.0f), glm::vec2(0.0f, 0.0f));
-	part2->SetMass(1.0f);
-
-	raven::Particle* part3 = new raven::Particle(glm::vec2(10.0f, 22.0f), glm::vec2(0.0f, 0.0f));
-	part3->SetMass(1.0f);
-
-	raven::ParticleSpring spring(part1, 0.1f, 10.0f);
-	registry.Register(part2, &spring);
-
-	raven::ParticleSpring sprin2(part2, 0.1f, 10.0f);
-	registry.Register(part3, &spring);
-
-	raven::ParticleSpring sprin3(part3, 0.1f, 10.0f);
-	registry.Register(part2, &spring);
-
-	registry.Register(part2, &gravitation);
-	registry.Register(part3, &gravitation);
-
-	registry.Register(part2, &fan);
-	registry.Register(part3, &fan);
-
-	particles.push_back(part2);
-	particles.push_back(part3);
 	/*
 	 * Setup variables needed for the Semi-Fixed timestep loop wiht interpolation
 	 */
 	float t = 0.0f;
-	float dt = 0.01f;
+	float dt = 0.0016f;
 
 	raven::Clock clock;
 	clock.Start();
@@ -70,12 +31,6 @@ int main()
 			{
 				window.close();
 			}
-
-			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::L)
-			{
-				static float val = 1.0f;
-				fan.SetStrength(val++);
-			}
 		}
 
 		float newTime = clock.GetElapsedTime().count();
@@ -93,13 +48,7 @@ int main()
 		 */
 		while (accumulator >= dt)
 		{
-			registry.UpdateForces(dt);
 			// Update physic world
-			for (size_t i = 0; i < particles.size(); i++)
-			{
-				particles[i]->Integrate(dt);
-			}
-
 			t += dt;
 			accumulator -= dt;
 		}
@@ -114,17 +63,6 @@ int main()
 		 * Render part of the game loop
 		 */
 		window.clear(sf::Color::White);
-
-		sf::CircleShape part;
-		part.setFillColor(sf::Color::Blue);
-		part.setRadius(10.0f);
-		part.setOrigin(10.0f, 10.0f);
-
-		for (size_t i = 0; i < particles.size(); i++)
-		{
-			part.setPosition(particles[i]->GetPosition().x, particles[i]->GetPosition().y);
-			window.draw(part);
-		}
 
 		window.display();
 	}
--- PhysicsEngine/Integrators.cpp
+++
@@ -1,6 +0,0 @@
-#include "Integrators.h"
-#include "Particle.h"
-
-void raven::math::EulerIntegrator(Particle& particle)
-{
-}
